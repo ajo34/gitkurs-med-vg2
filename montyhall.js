d2bb8933ee629674d7e6bc448fad9f63a2089913
ajo34 <addavadda@gmail.com>
2024-09-04 20:08:18 +0200

    huh

--- montyhall.js
+++ montyhall.js
@@ -1,6 +1,15 @@
 // Henter bildeelementene
 let dorbilderEl = document.querySelectorAll("img");
+let arr = []
+let array = ["https://assets.codepen.io/5037447/geit.png", "https://assets.codepen.io/5037447/bil.png", "https://assets.codepen.io/5037447/geit.png"];
+let possible = [0, 1, 2]
 
+document.getElementById("btn").addEventListener("click", function() {
+  for (let i = 0; i < 3; i++) {
+    console.log(i)
+    o()
+  }
+})
 // Går gjennom alle bildene og legger på samme lytter på hver av dem
 for (let i = 0; i < dorbilderEl.length; i++) {
   dorbilderEl[i].addEventListener("click", sjekkDor);
@@ -10,22 +19,25 @@ for (let i = 0; i < dorbilderEl.length; i++) {
 function sjekkDor(e) {
   let trykketDor = e.target;
   console.log(trykketDor.id);
+  console.log(trykketDor.src[38]);
   for (let i = 0; i < trykketDor.length; i++) {
 
   }
   // Bytter ut bilde av valgt dør med geit eller bil
   o(e)
 }
-let array = ["https://assets.codepen.io/5037447/geit.png", "https://assets.codepen.io/5037447/bil.png", "https://assets.codepen.io/5037447/geit.png"];
-let possible = [0, 1, 2]
 
-function o(e) {
+function o() {
   let num = Math.floor(Math.random() * possible.length)
-  e.target.src = array[possible[num]];
+  arr.push(array[possible[num]]);
   document.getElementById("a").innerHTML += possible[num]
-  if( num === 0 || num ===1) {
-    e.target.src = array[2];
-  }
+
   possible.splice(num, 1)
 
 }
+
+/*  if( num === 0 || num ===1) {
+  e.target.src = array[2];
+}
+e.target.src = 
+*/
